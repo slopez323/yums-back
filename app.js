fc670ac95935167409b948e5419524d3d38d2948
Sarah Lopez <slopez323@gmail.com>
2022-12-08 11:23:48 -0500

    url update for aws deployment

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(tokenVerif);
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/images", imagesRouter);
+app.use("/api/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/images", imagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
