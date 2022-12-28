3ab4bce183c2f0a9ae4e291776e9fc1b20e3ae49
Sarah Lopez <slopez323@gmail.com>
2022-12-27 20:45:33 -0500

    all cors

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ const cors = require("cors");
 // app.options("*", cors());
 
 app.use(
-  cors({
-    credentials: true,
-    origin: process.env.ORIGIN,
-  })
+  cors()
+  //   {
+  //   credentials: true,
+  //   origin: process.env.ORIGIN,
+  // }
 );
 
 var { mongoConnect } = require("./mongo.js");
